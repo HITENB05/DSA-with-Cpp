b3bbdec3cd0d4b1ce176875df8a6d6a7c1807a10
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-07-26 20:07:03 +0530

    msg

---
+++ src/trialforme.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+   using namespace std;
+   int main() {
+
+    float x;
+    cout<<"Enter a number: ";
+    cin>>x;
+
+    int y =(int)x;
+    if( x == y ) cout<<x<<" is an INTEGER"<<endl;
+    else cout<<x<<" is NOT an INTEGER"<<endl;
+
+
+
+
+
+   }
