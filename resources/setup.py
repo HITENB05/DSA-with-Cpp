2348d320248b381192b23e264c915d1d6664931a
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-07-27 17:57:52 +0530

    sui

--- resources/setup.py
+++ resources/setup.py
@@ -1,8 +1,12 @@
 import os 
-
+folder_name = input("Enter the folder name : ")
 file_name = input("Enter the file name : ")
 
-os.system(f"g++ src/{file_name}.cpp -o exes/{file_name}")
+os.system(f"g++ {folder_name}/{file_name}.cpp -o exes/{file_name}")
+print("output--->\n")
 os.system(f"./exes/{file_name}")
 
-print("ShellScript ran Sucessfully")
+print("\nShellScript ran Sucessfully\n")
+
+
+
