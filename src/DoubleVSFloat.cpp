259f581c5607407558e4e818e59e3b328f5227f6
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-07-27 12:11:44 +0530

    trying

---
+++ src/DoubleVSFloat.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+   using namespace std;
+   int main() {
+    double x = 3.14674764788513;
+    float y = 3.14674764788513;
+    cout<<x<<endl;
+    cout<<y<<endl;
+  
+
+
+   }
---
+++ src/FourDigitNumber.cpp
@@ -0,0 +1,16 @@
+include <iostream>
+   using namespace std;
+   int main() {
+    int n;
+    cout<<"Enter your Number";
+    cin>>n;
+
+    if ( n>=1000 &&  n<=9999 ) cout <<"Your number is a 4 digit number ";
+    else cout<<"Not a 4 digit number";
+    
+
+
+
+
+
+   }
