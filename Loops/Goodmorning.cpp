e8deaf6464a12f041af18578fef87e83a6d6c822
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-07-28 14:14:57 +0530

    Loops

---
+++ Loops/Goodmorning.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+   using namespace std;
+   int main() { 
+    int n;
+    cout<<"Enter a number :";
+    cin >>n;
+     
+    for (int i=1; i<=n;i++) {
+        cout<<"Hiten Bansal"<<endl;
+         
+    }
+   
+   
+   
+   
+   
+   
+   
+    // have to print gm 10 times
+
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+    //cout<<"Goodmorning"<<endl;
+   
+
+
+
+
+   }
