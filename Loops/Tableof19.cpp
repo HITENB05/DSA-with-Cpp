529c8f5b9830c5b512d62872015820d9e6c4792c
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-08-16 21:28:41 +0530

    cpp

---
+++ Loops/Tableof19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+   using namespace std;
+   int main() { 
+    // 1 ST METHOD
+    //for (int i=19 ; i<=190 ; i+=19){
+    //  cout<<i<<" \n";
+    for (int i=1 ; i<=10 ; i++){
+        cout<<i*19<<" \n";
+
+    }
+
+   }
