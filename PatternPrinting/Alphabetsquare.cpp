785e01f2168a0ea717169e3396e0a11895738eaa
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-08-20 20:58:46 +0530

    PatternPrinting

--- PatternPrinting/Alphabetsquare.cpp
+++ PatternPrinting/Alphabetsquare.cpp
@@ -5,12 +5,16 @@
     cout<<"Enter a number :";
     cin>>n;
 
-    for (int i=1;i<=n;i++)
-    {
+    for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-        
-        cout<<i<<" ";}
-         cout<<endl;
+            cout<<(j+64)<<" ";
+        }
+        cout<<endl;
     }
-   
+
+
+
+
+
+
    }
