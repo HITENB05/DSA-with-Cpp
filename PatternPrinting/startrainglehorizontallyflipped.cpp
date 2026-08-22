6ad5f3ae1121af4e43b8263b3c5625f4cb5dbf21
hiten bansal <hitenbansal@hitens-MacBook-Air.local>
2026-08-22 15:34:52 +0530

    PatternPrinting

---
+++ PatternPrinting/startrainglehorizontallyflipped.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+   using namespace std;
+   int main() { 
+    for ( int i=n;i>=1;i--){
+        for (int j=1;j=n+1-i;j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+
+    }
+   }
